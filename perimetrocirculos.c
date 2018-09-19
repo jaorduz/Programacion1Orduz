dc6f4d11e78b34ce41fb586d38a7c61862513746
CitlalliC <42586527+AfterlifeCC@users.noreply.github.com>
2018-09-19 10:38:23 -0500

    Add files via upload

---
+++ perimetrocirculos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+float main() {
+   float a;
+   float r1=12.5;
+   float d1=r1*2;
+   float p1=d1*3.1416;
+   printf ("El perimetro del circulo a es:%f", p1);
+   float b;
+   float r2=15.9;
+   float d2=r2*2;
+   float p2=d2*3.1416;
+   	printf ("El perimetro del circulo b es:%f", p2);
+   
+   }
---
+++ volcub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+float main() {
+   float a=9.8;
+   float x= a*a*a;
+   printf ("El volumen del cubo a es:%f", x);
+   float b=1.9;
+   float y= b*b*b;
+   printf ("El volumen del cubo b es:%f", y);
+   float c=1.1;
+   float z= c*c*c;
+   printf ("El volumen del cubo c es:%f", z);
+   }
